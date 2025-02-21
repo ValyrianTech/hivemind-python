e4e536b4d93a6329f8524e919ef6e02200402823
Wouter Glorieux <wouter@valyrian.tech>
2025-02-21 16:31:59 +0100

    Bump version to 0.2.0

--- src/hivemind/__init__.py
+++ src/hivemind/__init__.py
@@ -2,6 +2,8 @@
 Hivemind Protocol - A Condorcet-style Ranked Choice Voting System that stores all data on IPFS
 """
 
+__version__ = "0.2.0"
+
 from .issue import HivemindIssue
 from .option import HivemindOption
 from .opinion import HivemindOpinion
