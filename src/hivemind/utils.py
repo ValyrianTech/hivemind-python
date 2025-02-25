51b8616d482d27a0891a8188d6cc79481ec74432
Wouter Glorieux <wouter@valyrian.tech>
2025-02-25 15:24:39 +0100

    refactor(utils): Extract get_bitcoin_address function for reusability
    - Added new get_bitcoin_address function to convert private key to address
    - Updated generate_bitcoin_keypair to use the new function
    - Maintains 100% test coverage
    - Follows single responsibility principle

--- src/hivemind/utils.py
+++ src/hivemind/utils.py
@@ -5,6 +5,17 @@ from typing import Tuple
 from bitcoin.wallet import CBitcoinSecret, P2PKHBitcoinAddress
 from bitcoin.signmessage import BitcoinMessage, SignMessage
 
+def get_bitcoin_address(private_key: CBitcoinSecret) -> str:
+    """Get the Bitcoin address corresponding to a private key.
+    
+    Args:
+        private_key: Bitcoin private key
+        
+    Returns:
+        str: Bitcoin address in base58 format
+    """
+    return str(P2PKHBitcoinAddress.from_pubkey(private_key.pub))
+
 def generate_bitcoin_keypair() -> Tuple[CBitcoinSecret, str]:
     """Generate a random Bitcoin private key and its corresponding address.
     
@@ -16,7 +27,7 @@ def generate_bitcoin_keypair() -> Tuple[CBitcoinSecret, str]:
     private_key = CBitcoinSecret.from_secret_bytes(entropy)
     
     # Get the corresponding public address
-    address = str(P2PKHBitcoinAddress.from_pubkey(private_key.pub))
+    address = get_bitcoin_address(private_key)
     
     return private_key, address
 
