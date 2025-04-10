74f4abb130caafd2a7361125396eb283d12b129c
Wouter Glorieux <wouter@valyrian.tech>
2025-04-10 18:44:48 +0200

    removed test files

--- ipfstest.py
+++
@@ -1,7 +0,0 @@
-from ipfs_dict_chain.IPFS import connect
-from ipfs_dict_chain.IPFSDict import IPFSDict
-
-# connect(host='127.0.0.1', port=5001)
-data = IPFSDict(cid='Qmbu7h6JfTxHhv87QpFs9QahvuHorRVtwUriAzGakUeRP1')
-
-print(data)
